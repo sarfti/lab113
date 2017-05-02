dd2b7431db857c46e1dc2d98488296c52c3f4f82
student <student@2_VIT_LAB132_13.laboratory-132.ru>
2017-05-02 20:10:38 +0300

    Начало проекта.

---
+++ sarfti-timchenko/tim/src/tim/Pusk.java
@@ -0,0 +1,13 @@
+package tim;
+
+public class Pusk {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Здравствуйте=)");
+	}
+
+}
