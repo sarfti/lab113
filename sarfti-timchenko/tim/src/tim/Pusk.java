1f5754e5e4a4a15de1ac12cd18d54474053acd86
Mona Liza <student@2_vit_lab129_04.laboratory-129.ru>
2017-05-23 20:15:39 +0300

    Add message.

--- sarfti-timchenko/tim/src/tim/Pusk.java
+++ sarfti-timchenko/tim/src/tim/Pusk.java
@@ -7,7 +7,8 @@ public class Pusk {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Здравствуйте=)");
+		System.out.println("Здравствуйте=)");
+		System.out.println("Сортировка массива");
 	}
 
 }
